5027d3dac1152a9fc341c5b218ef72fa01c4d1ca
Adesh Partap Singh <106790533+dev1adesh@users.noreply.github.com>
2024-06-20 10:45:11 -0400

    Create BSM.h
    BSM.h file

---
+++ Black_Scholes_MonteCarlo_Engine/BSM.h
@@ -0,0 +1,42 @@
+#ifndef __Adesh__BSM__
+#define __Adesh__BSM__
+
+#include <iostream>
+
+class BSM
+{
+public:
+    BSM(float, float, float, float, float, long, long);
+    
+    ~BSM();
+    
+    float getBsmAsset();
+    float getBsmStrike();
+    float getBsmGrowth();
+    float getBsmVol();
+    float getBsmYears();
+    long getBsmSteps();
+    long getBsmMonteCarloSims();
+    
+    void logNormalRandomWalk();
+    
+    double getCallPrice();
+    double getPutPrice();
+    
+    double rn();
+
+private:
+
+    float bsmAsset;
+    float bsmStrike;
+    float bsmGrowth;
+    float bsmVol;
+    float bsmYears;
+    long bsmSteps;
+    long bsmMonteCarloSims;
+
+    double bsmCallPrice;
+    double bsmPutPrice;
+};
+
+#endif
